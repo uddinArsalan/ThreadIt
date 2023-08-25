e5d5462edbf9a0456c88d677c7db6359db98c43d
Arsalan <104685772+uddinArsalan@users.noreply.github.com>
2023-08-26 01:21:00 +0530

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -6,7 +6,7 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-const allowedOrigins = ["https://thread-it-one.vercel.app/"];
+const allowedOrigins = ["https://thread-it-one.vercel.app"];
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
