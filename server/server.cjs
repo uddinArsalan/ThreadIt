09b384a784c2bfe08ab585f95edf2ff22cbfd720
Arsalan <104685772+uddinArsalan@users.noreply.github.com>
2023-08-29 01:40:47 +0530

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-    origin: '"https://thread-it-one.vercel.app'
+    origin: 'https://thread-it-one.vercel.app'
 }));
 
 app.get("/", (req,res) => {
