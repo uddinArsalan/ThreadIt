c7c6a4ff912b32a318f09739ff01bb85b03752df
Arsalan <uddinarsalan91@gmail.com>
2024-02-14 01:47:23 +0530

    cors issue fixed

--- server/server.cjs
+++ server/server.cjs
@@ -7,9 +7,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.use(cors({
-  origin: '"https://thread-it-one.vercel.app'
-}));
+app.use(cors());
 
 app.get("/", (req,res) => {
   res.send('Server running successfully');
